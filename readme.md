8a0a1b63a46749a645485c77bce75ea572bf93e2
jasondavindev <jason.davin12@gmail.com>
2020-09-24 20:34:37 -0300

    add trigger

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Programação avançada de banco de dados - Lab 02
+
+## Participantes
+- Jason Carneiro
+- Marcos Paulo
+- Nicolas Fujisawa
+
---
+++ trigger.sql
@@ -0,0 +1,58 @@
+CREATE OR REPLACE TRIGGER chacabum BEFORE
+    INSERT ON employees
+    FOR EACH ROW
+DECLARE
+    rowcount NUMBER := 0;
+BEGIN
+    IF :new.department_id IS NOT NULL THEN
+        SELECT
+            COUNT(department_id)
+        INTO rowcount
+        FROM
+            departments
+        WHERE
+            department_id = :new.department_id;
+
+    END IF;
+
+    IF rowcount = 0 THEN
+        :new.department_id := 20;
+    END IF;
+END;
+
+SELECT
+    *
+FROM
+    employees
+ORDER BY
+    employee_id DESC
+FETCH NEXT 2 ROWS ONLY;
+
+INSERT INTO employees VALUES (
+    208,
+    'Joques',
+    'OConnell',
+    'joquesmail',
+    '650.507.9833',
+    '21-JUN-07',
+    'SH_CLERK',
+    2600,
+    NULL,
+    124,
+    NULL
+);
+
+INSERT INTO employees VALUES (
+    209,
+    'Jabinho',
+    'OConnell',
+    'jabinhomail',
+    '650.507.9833',
+    '21-JUN-07',
+    'SH_CLERK',
+    2600,
+    NULL,
+    124,
+    221
+);
+
